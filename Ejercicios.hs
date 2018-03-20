8d18d3547c75407c034916b121e845a9107b3005
semoraa <36653744+semoraa@users.noreply.github.com>
2018-03-20 10:26:21 -0500

    Add files via upload

--- Ejercicios.hs
+++ Ejercicios.hs
@@ -24,31 +24,54 @@ suma_digitos :: Int->Int->Int
 suma_digitos n < 10 = n
 suma_digitos n%10 + suma_digitos(n//10)
 
+--invertir entero
+invertir :: Integer->Integer->Integer
+invertir x y = if x== 0 then y else invertir(div x 10) ((y*10)+ (mod x 10))
+
+--palindromo
+palindromo :: Integer->String
+palindromo x = if x == (invertir x 0) then "es palindomo" else "no es palindomo"
+
 --invertir lista
 invertir_lista ::[Int]->[Int]
 invertir_lista [] = []
 invertir_lista (x:xs) = (invertirLista xs) ++ [x]
 
---sumar pares de una lista
-suma_pares :: [Int] -> 
-suma_pares [] = 0
-suma_pares (x:xs)
-|mod x 2 == 0 = x + sumaPares(xs)
-|otherwise = sumaPares(xs)
+--sumar pares lista RECURSIVO
+sumaParesRecursiva::[Int]->Int
+sumaParesRecursiva[] = 0
+sumaParesRecursiva (x:xs)
+ | mod x 2 == 1 = 0 + sumaParesRecursiva(xs)
+ | otherwise = x + sumaParesRecursiva(xs)
 
---cantidad impares de una lista
-cant_impar :: [int] -> Int
-cant_impar [] = 0
-cant_impar(x:xs)
-|mod x 2 == 1 = 1 + cant_impar(xs)
-|otherwise = cant_impar(xs)
+--sumar pares lista LISTA DE COMPRESION
+sumaParesCom::[Int]->Int
+sumaParesCom(x:xi) = sum (filter even (x:xi))
 
---determinar si una lista contiene a otra lista
-pertenece :: Int->[Int]-> Bool
-pertenece x [] = False
-pertenece x (y:ys)
-|x==y = True
-|otherwise = pertenece x ys
+--cantidad impares de una lista RECURSIVO
+cantImpar :: [int] -> Int
+cantImpar [] = 0
+cantImpar(x:xs)
+ | mod x 2 == 1 = 1 + cantImpar(xs)
+ | otherwise = cantImpar(xs)
+
+--cantidad impares de una lista LISTA DE COMPRENSION
+listaImparFunciones::[Int]->Int
+listaImparFunciones(x:xi) = length (filter odd (x:xi))
 
 
+--lista dentro de otra lista RECURSIVO
+contieneLista::[Int]->[Int]->Bool
+contieneLista [] [] = True
+contieneLista [] (y:ys) = True
+contieneLista (x:xs) [] = True
+contieneLista (x:xs) (y:ys) = lista y (x:xs) && contieneLista (x:xs) ys
 
+
+--mayor de una lista
+mayorLista::[Int]->Int
+mayorLista[] = 0
+mayorLista(x:[]) = x
+mayorLista(x:xs)
+ | x>mayorLista(xs) = x
+ | otherwise = mayorLista(xs)
